80772b99529fb24d2d9fc6417b93e1d702b8fed6
gesila233 <116056765+gesila233@users.noreply.github.com>
2023-10-18 15:35:29 +0800

    Add files via upload
    API使用练习

---
+++ API_Demo/test1.java
@@ -0,0 +1,17 @@
+package API_Demo;
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        //创建对象
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个小数");
+        //接收
+        //ctrl+alt+v 自动生成=左边的内容
+
+        double result = sc.nextDouble();
+        //输出
+        System.out.println(result);
+    }
+}
